c1594b0686ba4058327fc424af928abfd62c9a2d
Willian Yassunaka <willianhy@gmail.com>
2021-07-02 20:43:54 -0300

    show

--- src/Controller/VacinaController.js
+++ src/Controller/VacinaController.js
@@ -18,25 +18,25 @@ exports.post = (req, res, next) => {
   
  exports.getById = (req, res, next) => {
     let id = req.query.id;
-    let dateVaccine = new Date(req.query.dateVaccine);
 
+    var arrayDataAplicacaoRequest = req.query.dateVaccine.split("/");
+    var dateVaccine = new Date(arrayDataAplicacaoRequest[2], arrayDataAplicacaoRequest[1], arrayDataAplicacaoRequest[0]);    
     const collection = new Map();
 
-    collection.set("4120Z001", new Date(2020, 03, 29));
-    collection.set("4120Z004", new Date(2020, 04, 13));
-    collection.set("4120Z005", new Date(2020, 04, 14));
-    collection.set("CTMAV501", new Date(2020, 04, 30));
-    collection.set("CTMAV505", new Date(2020, 05, 31));
-    collection.set("CTMAV506", new Date(2020, 05, 31));
-    collection.set("CTMAV520", new Date(2020, 05, 31));
-    collection.set("4120Z025", new Date(2020, 06, 04));
-    
+    collection.set("4120Z001", new Date(2021, 03, 29));
+    collection.set("4120Z004", new Date(2021, 04, 13));
+    collection.set("4120Z005", new Date(2021, 04, 14));
+    collection.set("CTMAV501", new Date(2021, 04, 30));
+    collection.set("CTMAV505", new Date(2021, 05, 31));
+    collection.set("CTMAV506", new Date(2021, 05, 31));
+    collection.set("CTMAV520", new Date(2021, 05, 31));
+    collection.set("4120Z025", new Date(2021, 06, 04));    
     
     let dataVencimentoLote = collection.get(id);
     if(typeof dataVencimentoLote === "undefined") {
       res.json({"errorMessage":"Lote não pertence a lista divulgada"}).status(417);   
     } else {
-      let isVencida = +dataVencimentoLote > +dateVaccine;
+      var isVencida = +dateVaccine > +dataVencimentoLote; 
       res.json({"resultado":isVencida}).status(200);  
     }
  };
