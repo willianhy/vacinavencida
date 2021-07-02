2b8e0a5cea31d0bb8f8c90af20a50178b9880388
Willian Yassunaka <willianhy@gmail.com>
2021-07-02 20:02:22 -0300

    ..

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
  
 require('./src/Routes/index')(app); 
 
+app.use(express.static('public'))
 app.use(cors());
 app.use(express.json());
 app.listen(process.env.PORT || 5000);
--- src/Routes/VacinaRoute.js
+++ src/Routes/VacinaRoute.js
@@ -2,8 +2,4 @@ const VacinaController = require('../Controller/VacinaController');
 
 module.exports = (app) => {
    app.get('/vacina', VacinaController.getById);
-
-   app.get('/',function(req,res) {
-      res.sendFile('../index.html');
-    });
 }
